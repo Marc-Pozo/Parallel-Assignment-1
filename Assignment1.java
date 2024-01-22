8e930a137aa338db866973593eb07f5d1f861a3e
Marc23444 <marcelinopozo2001@gmail.com>
2024-01-22 16:23:27 -0500

    Initial Commit

---
+++ Assignment1.java
@@ -0,0 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+class Assignment1
+{
+
+    private static final int THREADS = 8;
+    private static final int LIMIT = 100000000;
+
+
+    public static void main(String []args)
+    {
+        
+    }
+};
